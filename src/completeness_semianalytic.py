4922e9efd8b3bedd014056eb67ca3f213794a948
lgbouma <lgbouma@mit.edu>
2017-10-01 17:23:32 -0400

    paper: minor edits

--- src/completeness_semianalytic.py
+++ src/completeness_semianalytic.py
@@ -1,25 +1,28 @@
+from scipy.integrate import trapz
 import matplotlib.pyplot as plt
 import numpy as np
-
-alpha = np.linspace(1,4,num=2000)
-f,ax=plt.subplots()
-ax.plot(alpha, (1-alpha)/alpha)
-
-f.savefig('alpha_power_allowed_vals.pdf',
-        bbox_inches='tight')
-
 
 alpha = 3.5
 f_d1c_upper = (1+0.1**alpha)**(-3)
 f_d1c_lower = 2**(-3)
 
-f_d1c = np.linspace(f_d1c_lower, f_d1c_upper, num=int(1e4))
+f_d1c = np.linspace(f_d1c_lower, f_d1c_upper, num=int(5e4))
 
-import IPython; IPython.embed()
-integrand = f_d1c**5 * (f_d1c**3 -1)**(-5/7)
+integrand = f_d1c**5 * (np.abs(f_d1c**3 -1))**(-5/7)
+
+norm = trapz(integrand, f_d1c)
+print('normalization is: {:.8f}'.format(norm))
+
+integrand /= norm
 
 f,ax=plt.subplots()
 ax.plot(f_d1c, integrand)
 
-f.savefig('temp.pdf',
+ax.set(
+xlabel='$f_{d1,c}$',
+ylabel='prob',
+yscale='log'
+)
+
+f.savefig('prob_fd1c.pdf',
         bbox_inches='tight')
